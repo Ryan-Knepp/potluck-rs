1dfe46214db7787068730a0099e0283f12196caa
Ryan Knepp <ryan.knepp@hey.com>
2025-04-04 19:26:33 -0500

    setup db, run migrations, and added seaorm entities to server

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
-use anyhow::{Ok, Result};
+mod entities;
+
 use axum::{Router, routing::get};
-use dotenvy::dotenv;
+use migration::{Migrator, MigratorTrait};
+use sea_orm::{Database, DatabaseConnection};
+use std::env;
+use tokio::net::TcpListener;
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    dotenv().ok();
+async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv()?;
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let db = Database::connect(db_url)
+        .await
+        .expect("Cannot connect to db");
+    Migrator::up(&db, None).await.unwrap();
 
-    let app = Router::new().route("/", get(root));
+    let state = AppState { db };
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let app = Router::new().route("/", get(root)).with_state(state);
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await?;
 
     Ok(())
+}
+
+#[derive(Clone)]
+struct AppState {
+    db: DatabaseConnection,
 }
 
 // basic handler that responds with a static string
